d6272780477916cd09940d3963953ae052d874d0
ksygzybygzy <150247762+ksygzybygzy@users.noreply.github.com>
2023-11-08 13:15:30 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1NgoOglX6Fgx0fvVrQndfu0BAQeMx-GoMX_RM5ekFkno/edit?usp=sharing';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTZJsuPceoodNAr9Ir8NXAeC4WkAKATHKDgkntn9jEVWmsvqCusDXKQJzBjabF2EHuU5G8MgyoUx1WH/pubhtml';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
